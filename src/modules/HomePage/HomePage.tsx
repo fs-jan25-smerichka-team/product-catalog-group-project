688db2ed128542299d57b37bfc53e01340b3f682
Daniil Trusov <daniiltrusov00@gmail.com>
2025-04-24 16:10:02 +0300

    fixed path to fonts and to ProductCard in HomePage.tsx

--- src/modules/HomePage/HomePage.tsx
+++ src/modules/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 import { Box, Grid, Typography } from '@mui/material';
 
-import { ProductCard } from '../Shared/ProductCard/ProductCard';
+import { ProductCard } from '../shared/ProductCard/ProductCard';
 
 export const HomePage: React.FC = () => {
   return (
