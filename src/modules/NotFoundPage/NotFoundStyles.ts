72b97f56bf1ddf7dd7cd1678d6c8f65aaa980569
Daniil Trusov <daniiltrusov00@gmail.com>
2025-04-25 14:21:04 +0300

    feat(PCP-44): removed unused styles from NotFoundStyles.ts

--- src/modules/NotFoundPage/NotFoundStyles.ts
+++ src/modules/NotFoundPage/NotFoundStyles.ts
@@ -16,28 +16,3 @@ export const pageTitleStyle: SxProps<Theme> = {
     desktop: '56px',
   },
 };
-
-export const backButtonStyle: SxProps<Theme> = {
-  color: '#89939A',
-  padding: 0,
-  background: 'none',
-  boxShadow: 'none',
-  textTransform: 'none',
-  minWidth: 'auto',
-  '&:hover': {
-    color: '#0f0f11',
-    background: 'none',
-  },
-};
-
-export const buttonContentStyle: SxProps<Theme> = {
-  display: 'flex',
-  alignItems: 'center',
-  gap: '4px',
-};
-
-export const buttonTextStyle: SxProps<Theme> = {
-  display: 'flex',
-  alignItems: 'center',
-  gap: '4px',
-};
