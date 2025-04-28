58fc2dd3ee85e289fc510d2479ca053d445a8c6c
Mariia Koval <m.kovalmaria@gmail.com>
2025-04-28 22:07:52 +0300

    add footer

--- src/modules/MainLayout/MainLayout.tsx
+++ src/modules/MainLayout/MainLayout.tsx
@@ -1,28 +1,9 @@
-import { Container, Typography, Box } from '@mui/material';
+import { Container, Box } from '@mui/material';
 
 import { Header } from '../Header/Header';
-import { FOOTER_HEIGHT } from '../../constants/constants';
+import { Footer } from '../Footer/Footer';
 
 // TODO: remove temporary component Footer
-
-const Footer = () => (
-  <Box
-    sx={{
-      bottom: 0,
-      left: 0,
-      right: 0,
-      height: `${FOOTER_HEIGHT}px`,
-      zIndex: 1100,
-      color: 'primary.contrastText',
-      bgcolor: '#222',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    }}
-  >
-    <Typography variant="h6">Footer</Typography>
-  </Box>
-);
 
 export const MainLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -48,3 +48,12 @@ export type CartItem = {
   price: number;
   quantity: number;
 };
+
+export const footerLinks = [
+  {
+    text: 'Github',
+    href: 'https://github.com/fs-jan25-smerichka-team/product-catalog-group-project',
+  },
+  { text: 'Contacts', href: '' },
+  { text: 'Rights', href: '' },
+];
