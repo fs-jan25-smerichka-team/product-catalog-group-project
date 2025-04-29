09c2f8f806b3c2c35332342434ce5eac7379787c
Daniil Trusov <daniiltrusov00@gmail.com>
2025-04-29 12:52:41 +0300

    fixed Header and removed vite.svg logo

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   AppBar,
@@ -38,7 +38,7 @@ export const Header: React.FC = () => {
     <>
       <HideOnScroll>
         <AppBar position="sticky" sx={headerStyles}>
-          <Toolbar disabledutters="true" sx={toolbarStyles}>
+          <Toolbar disableGutters={true} sx={toolbarStyles}>
             <Box sx={logoStyles}>
               <AppIcon />
             </Box>
