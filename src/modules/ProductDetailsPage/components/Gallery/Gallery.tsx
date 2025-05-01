67a4317dda3ef8de4ec873168ac1eacf5b9861d3
Mariia Koval <m.kovalmaria@gmail.com>
2025-05-01 17:30:37 +0300

    add gallery slider

--- src/modules/ProductDetailsPage/components/Gallery/Gallery.tsx
+++ src/modules/ProductDetailsPage/components/Gallery/Gallery.tsx
@@ -1,24 +1,86 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React, { useState } from 'react';
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
+import { Box, useTheme, useMediaQuery } from '@mui/material';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+
+// import { Swiper as SwiperClass } from 'swiper/types';
+
+//@ts-expect-error: Swiper CSS has no TS types
+import 'swiper/css';
+//@ts-expect-error: Swiper CSS has no TS types
+import 'swiper/css/navigation';
+//@ts-expect-error: Swiper CSS has no TS types
+import 'swiper/css/thumbs';
+//@ts-expect-error: Swiper CSS has no TS types
+import 'swiper/css/free-mode';
 
 import { ProductDetailsInfo } from '../../../../utils/Types';
+import {
+  containerStyle,
+  mainImageStyle,
+  paginationStyle,
+} from './GalleryStyles';
 
 type Props = {
   product: ProductDetailsInfo;
 };
 
 export const Gallery: React.FC<Props> = ({ product }) => {
+  const { images } = product;
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperClass | null>(null);
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.up('tablet'));
+
   return (
-    <>
-      {/* Тут буде Swiper */}
-      <Box>
-        <img
-          src={product.images[0]}
-          alt="product image"
-          width={288}
-          height={288}
-        />
-      </Box>
-    </>
+    <Box sx={containerStyle}>
+      {/* Thumbnails */}
+      <Swiper
+        onSwiper={setThumbsSwiper}
+        direction={isTablet ? 'vertical' : 'horizontal'}
+        slidesPerView="auto"
+        spaceBetween={'8px'}
+        watchSlidesProgress
+        allowTouchMove={false}
+        modules={[FreeMode, Navigation, Thumbs]}
+        style={{
+          height: 'fit-content',
+          order: isTablet ? 1 : 2,
+        }}
+      >
+        {images.map(image => (
+          <SwiperSlide
+            key={image}
+            style={{ height: 'auto', width: 'auto' }}
+            className="thumb-slide"
+          >
+            <Box
+              component="img"
+              src={image}
+              alt={image}
+              sx={paginationStyle(theme)}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Main Image */}
+      <Swiper
+        loop
+        thumbs={{ swiper: thumbsSwiper }}
+        modules={[FreeMode, Navigation, Thumbs]}
+        style={{
+          width: '100%',
+          height: '100%',
+          aspectRatio: '1 / 1',
+          order: isTablet ? 2 : 1,
+        }}
+      >
+        {images.map(image => (
+          <SwiperSlide key={image}>
+            <Box component="img" src={image} alt={image} sx={mainImageStyle} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </Box>
   );
 };
