90262808d3af52dac151b0e9e9bf68e31bbc9ac9
ENigmusSS <128493758+ENigmusSS@users.noreply.github.com>
2025-05-02 09:57:27 +0300

    fix/PCP-53_cart_fixes_bgfix

--- src/modules/CartPage/CartStyles.ts
+++ src/modules/CartPage/CartStyles.ts
@@ -3,5 +3,5 @@ import { SxProps, Theme } from '@mui/material/styles';
 export const CartGridStyle = (theme: Theme): SxProps<Theme> => ({
   maxWidth: '1136px',
   mx: 'auto',
-  bgcolor: theme.palette.primary.contrastText,
+  bgcolor: theme.palette.secondary.contrastText,
 });
