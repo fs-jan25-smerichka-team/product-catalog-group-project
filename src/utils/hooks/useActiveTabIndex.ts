4c3f730cdf70f88d42776b2982bf405a56a6fe6d
Daniil Trusov <daniiltrusov00@gmail.com>
2025-05-04 11:00:32 +0300

    add exception for contacts, rights and not-found page

--- src/utils/hooks/useActiveTabIndex.ts
+++ src/utils/hooks/useActiveTabIndex.ts
@@ -1,13 +1,22 @@
 import { useLocation } from 'react-router-dom';
 import { useMemo } from 'react';
 
+const headerPaths = [
+  '',
+  'home',
+  'phones',
+  'tablets',
+  'accessories',
+  'favourites',
+  'cart',
+];
+
 export const useActiveTabIndex = (titles: string[], icons: string[]) => {
   const location = useLocation();
 
   const allTabs = useMemo(() => [...titles, ...icons], [titles, icons]);
-
   const path = location.pathname.toLowerCase().split('/')[1] || '';
-  const index = allTabs.indexOf(path);
+  const index = headerPaths.includes(path) ? allTabs.indexOf(path) : -2;
 
   return index === -1 ? 0 : index;
 };
