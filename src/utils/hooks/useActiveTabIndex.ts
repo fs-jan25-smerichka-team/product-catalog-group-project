a71c3107a9fe76f4091609a487d174d7f428259b
nataliia-v <nataly.verbenskaya@gmail.com>
2025-05-04 10:41:28 +0300

    fix home tab

--- src/utils/hooks/useActiveTabIndex.ts
+++ src/utils/hooks/useActiveTabIndex.ts
@@ -1,17 +1,13 @@
-import { useEffect, useState, useMemo } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useMemo } from 'react';
 
 export const useActiveTabIndex = (titles: string[], icons: string[]) => {
   const location = useLocation();
-  const [activeIndex, setActiveIndex] = useState(-1);
 
   const allTabs = useMemo(() => [...titles, ...icons], [titles, icons]);
 
-  useEffect(() => {
-    const path = location.pathname.toLowerCase().split('/')[1] || '';
-    const index = allTabs.indexOf(path);
-    setActiveIndex(index);
-  }, [location.pathname, allTabs]);
+  const path = location.pathname.toLowerCase().split('/')[1] || '';
+  const index = allTabs.indexOf(path);
 
-  return activeIndex;
+  return index === -1 ? 0 : index;
 };
